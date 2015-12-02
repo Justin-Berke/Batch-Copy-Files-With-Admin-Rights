3357fd52e293eb7707e1d54d032331dc6c432b5b
Justin Berke <Justin.Berke+github@gmail.com>
2015-12-02 15:45:10 -0600

    Initial Push
    - Check for permissions
    - Set permissions
    - Delete an existing folder
    - Copy directory to the old folder's location

---
+++ Copy Full Directory Template.bat
@@ -0,0 +1,32 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+set copycmd=/y
+
+:: Delete entire directory & replace all contents
+rmdir /s /q "C:\inetpub\wwwroot\Application Directory\"
+xcopy "C:\Users\Justin Berke\Documents\Visual Studio 2013\Projects\Application Directory" "C:\inetpub\wwwroot\Application Directory" /E /I
